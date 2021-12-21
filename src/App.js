f4d01bcb1996956c9933946bc73db8f0591038b3
Alexander <81567588+sashacozub@users.noreply.github.com>
2021-12-21 15:43:35 +0100

    Normalize CSS and add base font-family

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
 
 function App() {
-  return <h1>Reddit Minimal</h1>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <button>Press me!</button>
+      <input type='text' />
+      <ul>
+        <li>123</li>
+      </ul>
+      <code>This is code</code>
+      <h3>H3</h3>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod iste
+        blanditiis dignissimos officiis asperiores placeat reprehenderit ut
+        nemo, ratione iure, eveniet eos rerum reiciendis sunt possimus! Placeat
+        expedita temporibus saepe quibusdam iusto et ratione corporis
+        consequatur, minima eum ipsa eos corrupti labore? Odit minus rerum
+        adipisci blanditiis. Autem, tempora eveniet?
+      </p>
+    </div>
+  );
 }
 
 export default App;
