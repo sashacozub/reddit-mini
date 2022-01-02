bf5cf8c13c063f0b2a7fea82c72ae1108d460421
Alexander <81567588+sashacozub@users.noreply.github.com>
2022-01-02 18:55:10 +0100

    Error page fix

--- src/features/ErrorPage/ErrorPage.js
+++ src/features/ErrorPage/ErrorPage.js
@@ -5,14 +5,14 @@ import './ErrorPage.css';
 
 const ErrorPage = () => {
   return (
-    <div className='error-page-ctr regular-box'>
+    <main className='error-page-ctr regular-box'>
       <h1>Opps! Something went wrong here!</h1>
       <h2>404 Not Found</h2>
       <br />
       <Link to='/'>
         <button className='button'>Go to the main page</button>
       </Link>
-    </div>
+    </main>
   );
 };
 
